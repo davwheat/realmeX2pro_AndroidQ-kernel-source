22364d2b227f681d81c7788a41b6ce42311b2c45
David Wheatley <davidwheatley03@gmail.com>
2020-04-29 03:08:17 +0100

    :sparkles: Move to Clang cross compiler

--- build.sh
+++ build.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-# set build architecture to ARM64
-export ARCH=arm64 && export SUBARCH=arm64
+echo
 
-# set prebuild toolchain directory
-export CROSS_COMPILE=`pwd`/toolchain64/bin/aarch64-linux-android-
+echo -e "\n"
+echo -e "############################"
+echo -e "#      BUILD STARTING      #"
+echo -e "############################"
+echo -e "\n"
 
+echo -e "\n\nCreating output directory...\n"
 # create output directory
 mkdir -p out
 
-# prevent errors from stopping script
-set -e
+echo -e "\n\nPreparing PATH...\n"
+# add prebuilts to path for duration of this script
+PATH="$(pwd)/prebuilts/clang/bin:$(pwd)/prebuilts/gcc/bin:${PATH}"
 
+echo -e "\n\nCleaning up last build...\n"
 # clean up last (attempted) build
 make O=out mrproper
 
+echo -e "\n\nPreparing configuration file...\n"
 # prepare for build with specified defconfig
 # arch/arm64/configs/defconfig
-make O=out defconfig
+make O=out ARCH=arm64 sm8150-perf_defconfig
 
+echo -e "\n\nStart building kernel source...\n"
 # start the build
 # (and hope to fucking god it bloody works...)
-make O=out -j$(nproc --all)
+make O=out \
+     ARCH=arm64 \
+     CC=clang \
+     CLANG_TRIPLE=aarch64-linux-gnu- \
+     CROSS_COMPILE=aarch64-linux-android-
 
-set +e
-
-echo "\n"
-echo "############################"
-echo "#      BUILD FINISHED      #"
-echo "############################"
-echo "\n"
+echo -e "\n"
+echo -e "############################"
+echo -e "#      BUILD FINISHED      #"
+echo -e "############################"
+echo -e "\n"
